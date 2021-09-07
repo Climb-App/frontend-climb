8758a31be2a994af2bd0b002718d232185e9bfb7
CarlosDeveloperr <luis9620@hotmail.com>
2021-09-06 22:04:55 -0500

    Cambio Post Login

--- components/commons/input.js
+++ components/commons/input.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-export default function Input({ name, type, label, place }) {
+export default function Input({ name, type, label, place, id }) {
   return (
     <>
       <label className="title label" htmlFor={`${name}`}>{`${label}`}</label>
       <input
+        id={`${id}`}
         className="input"
         type={`${type}`}
         name={`${name}`}
