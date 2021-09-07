0a01466081443913f0423f6a7f343a7f7d5e92ec
CarlosDeveloperr <luis9620@hotmail.com>
2021-09-06 21:36:18 -0500

    profile Template

--- pages/workspace/index.js
+++ pages/workspace/index.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import MainLayoutComponent from '../../components/MainLayout/index';
-import Cards from '../../components/commons/cards'
-import Title from '../../components/commons/title'
-import axios from 'axios'
-import BASE_URL from '../../services/api'
-
-
+import React from "react";
+import { useState, useEffect } from "react";
+import MainLayoutComponent from "../../components/MainLayout/index";
+import Cards from "../../components/commons/cards";
+import Title from "../../components/commons/title";
+import axios from "axios";
+import BASE_URL from "../../services/api";
 
 export default function Workspace() {
-
   // const client = axios.create({
-  //   baseURL: `${BASE_URL}workspaces/` 
+  //   baseURL: `${BASE_URL}workspaces/`
   // });
-  
+
   //   const [post, setPost] = React.useState(null);
 
   //   React.useEffect(() => {
@@ -24,21 +22,17 @@ export default function Workspace() {
   //     getPost();
   //   }, []);
 
-
-
-
-
   return (
     <>
       <MainLayoutComponent>
-
-         <Title> {/*Aqui va el contenido de workspace*/ } 
+        <Title>
+          {" "}
+          {/*Aqui va el contenido de workspace*/}
           {/* {post?.[0]?.name ?? "Vacio" }
          <p> {post?.[0]?.description ?? "Vacio" }</p> */}
         </Title>
         <Cards>{/*Contenido de los workspaces */}</Cards>
-
       </MainLayoutComponent>
     </>
-  )
+  );
 }
