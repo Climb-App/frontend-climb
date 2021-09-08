3159d5a9eacc29e24b173c3a1998db542eb086eb
CarlosDeveloperr <luis9620@hotmail.com>
2021-09-08 16:54:39 -0500

    Creacion de hooks Profile

---
+++ hooks/useUser.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+import { getToken } from "../services/operationsTokens";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { BASE_URL } from "../services/api";
+
+const useUser = () => {
+  const [User, setUser] = useState(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    async function getUser() {
+      try {
+        const response = await axios.get(`${BASE_URL}api/v1/user/`, {
+          headers: {
+            Authorization: getToken(),
+          },
+        });
+        setUser(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
+        // setError(error)
+        if (error.response.status >= 402 && error.response.status <= 500) {
+          toast.error("Error Cliente o Servidor ");
+          console.log("Error");
+          router.push("/");
+        }
+        if (error.response.status == 401) toast.error("Unauthorized");
+      }
+    }
+    getUser();
+  }, [router]);
+
+  return User;
+};
+
+export default useUser;
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -2,90 +2,18 @@ import React from "react";
 import MainLayoutComponent from "../../components/MainLayout/index";
 import { Form, Button } from "react-bootstrap";
 import { toast } from "react-toastify";
-import axios from "axios";
-import { right } from "@popperjs/core";
-import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import { BASE_URL } from "../../services/api";
-import { getToken } from "../../services/operationsTokens";
+import useUser from "../../hooks/useUser";
+import useUserData from "../../hooks/useUserData";
 
 export default function PerfilUsuario() {
+  const User = useUser();
+  const UserData = useUserData(User);
+
   const router = useRouter();
   const ResetPass = () => {
     router.push("/recovery-pass/");
   };
-
-  const [User, setUser] = useState(null);
-  const [UserData, setUserData] = useState(null);
-
-  // const baseUrlConnect = "http://127.0.0.1:8000/api/user/";
-
-  useEffect(() => {
-    async function getUser() {
-      try {
-        const response = await axios.get(`${BASE_URL}api/v1/user/`, {
-          headers: {
-            Authorization: getToken(),
-          },
-        });
-        setUser(response.data);
-        console.log(response.data);
-      } catch (error) {
-        console.log(error);
-        // setError(error)
-        if (error.response.status >= 402 && error.response.status <= 500) {
-          toast.error("Error Cliente o Servidor ");
-          console.log("Error");
-          router.push("/");
-        }
-        if (error.response.status == 401) toast.error("Unauthorized");
-      }
-    }
-    getUser();
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  useEffect(() => {
-    if (!User) {
-      return;
-    }
-    console.log(User);
-    console.log("La informacion traida y guarda en User es: " + User[0].role);
-
-    const UserRole = User?.[0]?.role == 1 ? "admin/" : "member/";
-
-    console.log("El role es:" + UserRole);
-
-    async function getData() {
-      try {
-        const response = await axios.get(
-          `${BASE_URL}api/v1/user/${UserRole}${User[0].id}`,
-          {
-            headers: {
-              Authorization: getToken(),
-            },
-          }
-        );
-        setUserData(response.data);
-        console.log(response.data);
-        toast.success(
-          "Acceso a Informacion del Usuario: " + `${response.data.name}`
-        );
-      } catch (error) {
-        console.log(error);
-        // setError(error)
-        if (error.response.status >= 402 && error.response.status <= 500) {
-          toast.error("Error Cliente o Servidor ");
-          console.log("Error");
-          router.push("/");
-        }
-        if (error.response.status == 401) toast.error("Unauthorized");
-      }
-    }
-    getData();
-    console.log("La informacion traida y guarda en Data es: " + UserData?.[0]);
-  }, [User]);
 
   return (
     <>
